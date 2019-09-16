06aa3fb9a922ab158cce1b1976dda6d5ba621a2a
Daniel I <danielionita88@yahoo.com>
2019-09-16 20:52:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,6 +19,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
-  
+  min = 0 
+  array.length.times do |count|
+    if array[count] < min
+      min = array[count]
+    end
+  end
+  min
+
 end
